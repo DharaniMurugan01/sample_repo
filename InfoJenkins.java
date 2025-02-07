a5d84ebe029f455eaf215a658b506b22009385ad
DharaniMurugan01 <2k21dhara@gmail.com>
2025-02-07 06:55:09 +0530

    Add files via upload

---
+++ InfoJenkins.java
@@ -0,0 +1,8 @@
+public class InfoJenkins {
+    public static void main(String[] args) {
+        String name = System.getenv("NAME");
+        int age = Integer.parseInt(System.getenv("AGE"));
+        float weight = Float.parseFloat(System.getenv("WEIGHT"));
+        System.out.println("Hello, " + name + "! You are " + age + " years old and weigh " + weight + " kg.");
+    }
+}
